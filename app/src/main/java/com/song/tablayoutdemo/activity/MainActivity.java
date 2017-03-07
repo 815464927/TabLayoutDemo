cb34d258a527a6c3d5d817a85cd7ac3f48e6cfbf
Song <1034966089@qq.com>
2017-03-07 15:21:45 +0800

    删除一些空格
    delete some space

--- app/src/main/java/com/song/tablayoutdemo/activity/MainActivity.java
+++ app/src/main/java/com/song/tablayoutdemo/activity/MainActivity.java
@@ -85,7 +85,6 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
